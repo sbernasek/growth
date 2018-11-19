7a9c2b13fe58f380a94552f9b24da4c7cc2bdada
Sebastian Bernasek <sebastian@u.northwestern.edu>
2018-11-18 23:06:45 -0800

    changed sweep basis

--- growth/sweep/sweep.py
+++ growth/sweep/sweep.py
@@ -21,6 +21,48 @@ class Sweep(Batch):
         return np.linspace(0.0, 1., self.density)
 
     @property
+    def recombination_duration(self):
+        """ Fraction of growth period subject to recombination. """
+        return np.linspace(0.0, 1., self.density)
+
+    @property
     def grid(self):
         """ Meshgrid of division and recombination rates. """
-        return np.meshgrid(self.division, self.recombination, indexing='xy')
+        return np.meshgrid(self.recombination_duration, self.recombination, indexing='xy')
+
+    @classmethod
+    def build_simulation(cls, parameters, simulation_path, **kwargs):
+        """
+        Builds and saves a simulation instance for a set of parameters.
+
+        Args:
+
+            parameters (iterable) - parameter sets
+
+            simulation_path (str) - simulation path
+
+            kwargs: keyword arguments for GrowthSimulation
+
+        """
+
+        # parse parameters
+        recombination_duration, recombination = parameters
+
+        # instantiate simulation
+        final_population = 2**12
+        recombinant_population = recombination_duration*final_population
+
+        simulation = GrowthSimulation(
+            division=1.0,
+            recombination=recombination,
+            recombinant_population=recombinant_population,
+            final_population=final_population,
+            **kwargs)
+
+        # create simulation directory
+        if not isdir(simulation_path):
+            mkdir(simulation_path)
+
+        # save simulation
+        simulation.save(simulation_path)
+
